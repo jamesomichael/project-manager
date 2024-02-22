843ad2e56bffc938eac88404f24fcb4880e36653
James Michael <jomichael1994@gmail.com>
2024-02-22 20:34:08 +0000

    initial project work

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,23 @@
+import Input from './Input';
+
+const NewProject = () => {
+	return (
+		<div className="w-[35rem] mt-16">
+			<menu className="flex items-center  justify-end gap-4 my-4">
+				<li>
+					<button>Cancel</button>
+				</li>
+				<li>
+					<button>Save</button>
+				</li>
+			</menu>
+			<div>
+				<Input label="Title" />
+				<Input label="Description" textarea />
+				<Input label="Due Date" />
+			</div>
+		</div>
+	);
+};
+
+export default NewProject;
