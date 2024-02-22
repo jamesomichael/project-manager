63c7ad955bfa4879a668e28f7e822c654fff5880
James Michael <jomichael1994@gmail.com>
2024-02-22 20:50:30 +0000

    further styling work, add default section

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Button from './Button';
 
 const ProjectsSidebar = () => {
 	return (
@@ -7,9 +7,7 @@ const ProjectsSidebar = () => {
 				Your Projects
 			</h2>
 			<div>
-				<button className="px-4 py-2 text-xs md:text-base bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-					+ Add Project
-				</button>
+				<Button>+ Add Project</Button>
 			</div>
 			<ul></ul>
 		</aside>
