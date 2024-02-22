6383f2a10339a043fd14ffb05d4853ef6b7e2769
James Michael <jomichael1994@gmail.com>
2024-02-22 21:21:24 +0000

    initial state work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+
 import ProjectsSidebar from './components/ProjectsSidebar';
-// import NewProject from './components/NewProject';
+import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
-function App() {
+const App = () => {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	});
+
+	const handleProjectCreation = () => {
+		setProjectsState((prevState) => ({
+			...prevState,
+			selectedProjectId: null,
+		}));
+	};
+
 	return (
 		<main className="h-screen my-8 flex gap-8">
-			<ProjectsSidebar />
-			<NoProjectSelected />
+			<ProjectsSidebar onCreation={handleProjectCreation} />
+			{/* {content} */}
+			{projectsState.selectedProjectId === null ? (
+				<NewProject />
+			) : projectsState.selectedProjectId === undefined ? (
+				<NoProjectSelected onCreation={handleProjectCreation} />
+			) : undefined}
 		</main>
 	);
-}
+};
 
 export default App;
